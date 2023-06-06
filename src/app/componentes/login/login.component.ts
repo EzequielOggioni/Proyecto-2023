42eb0f1399ba6d046aa27555a80bafa7ff2b2458
Ezequiel Oggioni <oggioni@providencia.com.ar>
2023-06-06 00:16:32 -0300

    jwt decode

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import {  Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Usuario } from 'src/app/entidades/usuario';
 import { TokenGuard } from 'src/app/Guard/token.guard';
 import { ProductosService } from 'src/app/servicios/productos.service';
@@ -28,6 +29,8 @@ export class LoginComponent {
     .subscribe(data=>{
       
         localStorage.setItem('UsuarioLogueado',JSON.stringify(data));
+        var usuario = (this.jwt.decodeToken((<any>data).token)).data;
+        console.info(usuario);
         this.token.salida =true;
         this.route.navigateByUrl("bienvenido") ;
       
@@ -39,7 +42,7 @@ quitar(){
 } 
   constructor(public route: Router, 
     public otronombredevariable:ProductosService,
-    public token:TokenGuard, public http:HttpClient ) {
+    public token:TokenGuard, public http:HttpClient, public jwt:JwtHelperService ) {
   }
 
 }
